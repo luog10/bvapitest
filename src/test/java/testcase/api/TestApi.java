c9fdea01e733deaf4d2732a2c96b14be896fecee
luo.gang <luog10@qq.com>
2018-03-08 13:02:24 +0800

    Initial commit

---
+++ src/test/java/testcase/api/TestApi.java
@@ -0,0 +1,16 @@
+import apiutility.HttpRequest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestApi {
+    @Test
+    /*
+     *   测试不带参数的Get http请求
+     */
+    public void TestGetRequestForNoParameterRequest(){
+        //发送 GET 请求
+        String TestUrl = "http://10.21.10.179:9000/";
+        String s= HttpRequest.sendGet(TestUrl);
+        Assert.assertTrue(s.contains("九九"));
+    }
+}
---
+++ src/test/java/testcase/demo/TestDemo.java
@@ -0,0 +1,13 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestDemo {
+    @Test
+    public void testcase1(){
+        Assert.assertEquals(1,1);
+    }
+    @Test
+    public void testcase2(){
+        Assert.assertEquals(1,1);
+    }
+}
